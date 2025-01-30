768bb742854ec9b167a597e7e3ee502079f1b4b7
[kenanao tsimanyane] <[ketsimanyane@gmail.com]>
2025-01-30 09:58:54 +0200

    update nextConfig.mjs to load assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   basePath: "/redbulb",
-  assetPrefix: "/redbulb",
-  trailingSlash: true,
 };
 
 export default nextConfig;
