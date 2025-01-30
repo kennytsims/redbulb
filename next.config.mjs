812248b11d8373f4e3a2bb3e834ec0bec392dc30
[kenanao tsimanyane] <[ketsimanyane@gmail.com]>
2025-01-30 09:12:07 +0200

    updating nextConfig.mjs to load static assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/redbulb",
+  assetPrefix: "/redbulb",
+  trailingSlash: true,
 };
 
 export default nextConfig;
